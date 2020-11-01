85fcb5c5198d1fae6cfcd348ed95f8de7f9375ce
EditYJ <158392613@qq.com>
2020-11-01 14:51:55 +0800

    Spring aop尝试自定义切面类

---
+++ spring-06-aop/src/main/java/com/edityj/diylog/Log.java
@@ -0,0 +1,11 @@
+package com.edityj.diylog;
+
+public class Log {
+  public void beforeLog(){
+    System.out.println("=======方法执行前=======");
+  }
+
+  public void afterLog(){
+    System.out.println("=======方法执行后=======");
+  }
+}
